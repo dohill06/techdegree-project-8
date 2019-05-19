0c7190fd98615ba6ec72998d8248cb9b3f8c07f0
Douglas Hill <dohill06@gmail.com>
2019-05-19 09:57:41 -0700

    Set up routes

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const sequelize = require('./models').sequelize;
+
+
 const app = express();
 
 
 
+app.get('/', (req, res) => {
+    res.redirect('/books');
+});
 
+app.get('/books', (req, res) => {
 
+});
+
+app.get('/books/new', (req, res) => {
+
+});
+
+app.post('/books/new', (req, res) => {
+
+});
+
+app.get('/books/:id', (req, res) => {
+
+});
+
+app.post('/books/:id', (req, res) => {
+
+});
+
+app.get('/books/:id/delete', (req, res) => {
+
+});
 
 
 
