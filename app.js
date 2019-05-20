611265396604a5994423eb2e32493dfabdfcbf1a
Douglas Hill <dohill06@gmail.com>
2019-05-20 14:48:25 -0700

    Handle 404 errors

--- app.js
+++ app.js
@@ -63,6 +63,12 @@ app.post('/books/:id/delete', (req, res) => {
     });
 });
 
+app.use((req, res, next) => {
+    const err = new Error('Page Not Found');
+    err.status - 404;
+    next(err);
+});
+
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
